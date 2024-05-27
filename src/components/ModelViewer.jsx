509b2971a45a3646289ea9c34d7986d7dbfcdf85
ishanisarkar25 <132077616+ishanisarkar25@users.noreply.github.com>
2024-05-27 15:59:38 +0530

    updates for viewing the model

--- src/components/ModelViewer.jsx
+++ src/components/ModelViewer.jsx
@@ -1,8 +1,19 @@
-import React from "react";
-
-const ModelViewer = () => {
-  return(<div>
-    model</div>);
+import { PerspectiveCamera, View } from "@react-three/drei";
+import Lights from "./Lights";
+import { Suspense } from "react";
+import IPhone from './IPhone'
+const ModelViewer = (index,groupRef,controlRef,gsapType,setRotationState,size,item) => {
+  return(<View
+    index={index}
+    id={gsapType}
+    className={`border-2 border-red-500 w-full h-full ${index===2}? 'right-[-100%]' : ''`}>
+  {/*Ambient light*/}
+  <ambientLight intensity={0.3}/>
+  <PerspectiveCamera makeDefault position={[0,0,4]}/>
+  <Lights/>
+  <Suspense fallback={<div>Loading</div>}>
+  <IPhone/></Suspense>
+    </View>);
 };
 
 export default ModelViewer;
